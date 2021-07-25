9c630eb3927dcd1ac199473c7469a299053befa1
Allan Frese <allan@frese.com>
2021-07-25 09:19:05 +0200

    improved tests

--- cmd/jgrep_test.go
+++ cmd/jgrep_test.go
@@ -1,29 +1,77 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"strings"
 	"testing"
 )
 
-
+var testSets = []struct{
+    data []byte
+    path string
+    result []byte
+}{
+    {
+        data: []byte(`{"key": "value"}`),
+        path: "key",
+        result: []byte(`"value"`),
+    }, 
+    {
+        data: []byte(`{"key": "value", "key2": "value2"}`),
+        path: "key",
+        result: []byte(`"value"`),
+    }, 
+    {
+        data: []byte(`{"hash": { "key": "value"} }`),
+        path: "hash/key",
+        result: []byte(`"value"`),
+    }, 
+    {
+        data: []byte(`{"hash": [ { "key": "value1"}, { "key": "value2" } ]}`),
+        path: "hash/*/key",
+        result: []byte(`["value1","value2"]`),
+    }, 
+    {
+        data: []byte(`{"hash": [ { "key": "value1"}, { "key": "value2" }, { "notkey": "value3" } ]}`),
+        path: "hash/*/key",
+        result: []byte(`["value1","value2"]`),
+    }, 
+    {
+        data: []byte(`{"hash": { "subhash1": { "key": "value1"}, 
+                                 "subhash2": { "key": "value2" }, 
+                                 "subhash3": { "notkey": "value3" } }}`),
+        path: "hash/*/key",
+        result: []byte(`["value1","value2"]`),
+    }, 
+    {
+        data: []byte(`{"hash": [ {"cat": "color", "color": "red"}, {"cat": "food", "fruit": "apple"}, {"cat": "color", "color": "blue"} ]}`),
+        path: "hash/cat=color",
+        result: []byte(`[{"cat":"color","color":"red"},{"cat":"color","color":"blue"}]`),
+    }, 
+    {
+        data: []byte(`{"hash": [ {"cat": "color", "color": "red"}, {"cat": "food", "fruit": "apple"}, {"cat": "color", "color": "blue"} ]}`),
+        path: "hash/cat=color/color",
+        result: []byte(`["red","blue"]`),
+    }, 
+}
 func TestJgrep(t *testing.T) {
 
-	var source interface{}
+    var source interface{}
 
-	data := []byte(`{"key": "value"}`)
-	path := []string{ "key" }
-
-	err := json.Unmarshal(data, &source)
-	if err != nil {
-		t.Fatal("Cannot Unmarshal json data")
-	}
-	res := jgrep(source, path)
-	out, err := json.Marshal(res)
-    if err != nil {
-        t.Fatal("Cannot Marshal result")
+    for _, set := range testSets {
+        err := json.Unmarshal(set.data, &source)
+        if err != nil {
+            t.Fatal("Cannot Unmarshal json data")
+        }
+        res := jgrep(source, strings.Split(set.path, "/"))
+        out, err := json.Marshal(res)
+        if err != nil {
+            t.Fatal("Cannot Marshal result")
+        }
+        match := bytes.Compare(out, set.result)
+        if match != 0 {
+            t.Errorf("Got <%s> but wanted <%s>", string(out), string(set.result))
+        }
     }
-	if string(out) != "\"value\"" {
-		t.Errorf("Got %s but wanted 'value'", string(out))
-	}
-
 }
